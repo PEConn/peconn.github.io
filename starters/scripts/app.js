2c2e4724334fab14cebaf060db9f15dddf23f7b2
Peter E Conn <peconn@google.com>
2018-05-23 13:43:20 +0100

    Add a Web Worker. Remove blindly copied licenses.

--- starters/scripts/app.js
+++ starters/scripts/app.js
@@ -1,18 +1,3 @@
-// Copyright 2016 Google Inc.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//      http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-
 (function() {
   'use strict';
   const configs = [
@@ -53,6 +38,22 @@
     if (Notification.permission !== "granted") {
       Notification.requestPermission();
     }
+  }
+
+  if (window.Worker) {
+    const button = document.getElementById('butMessage');
+    const output = document.getElementById('response');
+    button.disabled = false;
+
+    var worker = null;
+
+    button.addEventListener('click', function() {
+      if (worker == null) {
+        worker = new Worker('./math-worker.js');
+        worker.onmessage = (event) => response.textContent = event.data;
+      }
+      worker.postMessage([3]);
+    });
   }
 
 })();
